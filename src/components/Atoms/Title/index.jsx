3bd8f9ca703e3e473a061f18f037854c6bd0661d
A S <as@enoMacBook-Air.local>
2025-02-01 17:19:16 +0900

    余分な空白行の消去

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -5,16 +5,13 @@ import FONTFAMILY from "../../../variables/font_family";
 import styled from "styled-components";
 import React from "react";
 
-
 export const Title = () => {
   return <StyledText>SIMPLE TODO APP</StyledText>;
-}
-
+};
 
 const StyledText = styled.div`
-
   color: ${COLOR.WHITE};
-  ${TEXT.L}; 
+  ${TEXT.L};
   font-family: ${FONTFAMILY.NOTO_SANS};
   @media (max-width: ${BREAKPOINT.MEDIUM}) {
     ${TEXT.M};

23c950121c0f38015100c5f19ec152835777d3a4
A S <as@enoMacBook-Air.local>
2025-02-02 17:39:33 +0900

    EditButtonのコード修正

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -4,15 +4,27 @@ import COLOR from "../../../variables/color";
 import pencil from "../../../assets/svg/pencil.svg";
 
 export const EditButton = () => {
-  return <StyledButton onClick={() => console.log("clicked")} />;
+  return (
+    <StyledButton onClick={() => console.log("clicked")}>
+      <img src={pencil} alt="pencil" />
+    </StyledButton>
+  );
 };
 
 const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 20px;
   height: 20px;
   border: none;
   border-radius: 50%;
-  background: url(${pencil}) no-repeat center/contain;
+  background-color: ${COLOR.GREEN_TRANSPARENT};
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
 
   &:hover {
     background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};

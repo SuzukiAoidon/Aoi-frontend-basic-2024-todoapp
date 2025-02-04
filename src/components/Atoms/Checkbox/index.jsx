73003169903b2a5dda59f069b14d615266e8b290
A S <as@enoMacBook-Air.local>
2025-02-04 22:18:00 +0900

    Checkboxにtransitionを追加

---
+++ src/components/Atoms/Checkbox/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import check from "../../../assets/svg/check.svg";
+
+export const Checkbox = () => {
+  return (
+    <StyledButton onClick={() => console.log("clicked.")}>
+      <StyledImg src={check} alt="checked" />
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  background-color: transparent;
+  border: 2px solid ${COLOR.LIGHT_GRAY};
+  border-radius: 2px;
+
+  &:hover img {
+    opacity: 1;
+  }
+`;
+
+const StyledImg = styled.img`
+  transition: all 0.2s ease;
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+`;
